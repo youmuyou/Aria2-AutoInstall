a4d83557b3e1bddb4292fdeef39aadfe81a77613
sxuecks <sxueck@outlook.com>
2019-02-07 01:46:48 +0800

    添加dht协议支持

--- install.ps1
+++ install.ps1
@@ -18,13 +18,16 @@ New-Variable -Name ConfigFile -Value "oriAria2Config.conf" -Option Constant
 #main
 "欢迎进入Aria2的Windows端安装与配置程序"
 
+$desktop = [Environment]::GetFolderPath("Desktop")
+
 $choice = Read-Host "你需要安装(y)还是卸载(n)"
 if ($choice -eq "n") {
     "正在结束后台程序"
     Get-Process -Name aria2c | Stop-Process
     "正在删除文件目录"
     Remove-Item -Recurse -Force "C:\Aria2Download"
-    Remove-Item -Force "$Env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\Aria2StartUp.ps1"
+    Remove-Item -Force "$Env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\Aria2StartUp.vbs"
+    Remove-Item -Force "$desktop\Aria2下载器.lnk"
     "卸载完成"
     Pause
     exit
@@ -36,7 +39,6 @@ Start-Sleep -Seconds 1
 
 "初始化..."
 #value init
-$desktop = [Environment]::GetFolderPath("Desktop")
 #$PSDefaultParameterValues['Out-File:Encoding'] = 'utf8'
 (Invoke-webrequest -URI "http://tracker.tbuu.xyz:85/").Content > BackupTracker.txt
 #cmd /c ftype Microsoft.Powershellscript.1="C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" "%1"
@@ -64,6 +66,7 @@ if ([String]::IsNullOrEmpty($downDIR)) {
 "接下来生成配置文件"
 
 $dirpath = "C:\Aria2Download"
+$dhtfile = "$dirpath\config\dht.dat"
 
 #write config file
 "daemon=true" > $ConfigFile
@@ -95,6 +98,7 @@ $dirpath = "C:\Aria2Download"
 "bt-seed-unverified=true" >> $ConfigFile
 "allow-piece-length-change=true" >> $ConfigFile
 "bt-force-encryption=true" >> $ConfigFile
+"dht-file-path=$dhtfile" >> $ConfigFile
 
 #trackerlist = Get-Content BackupTracker.txt
 #"bt-tracker=$tracker" >> $ConfigFile
@@ -110,6 +114,7 @@ Copy-Item -force ".\bin\aria2c.exe" $dirpath\
 New-Item -ItemType file "$dirpath\config\aria2.session"
 Copy-Item -force .\oriAria2Config.conf "$dirpath\config\"
 Copy-Item .\bin\dos2unix.exe $dirpath -Force
+Copy-Item -Force .\dht.dat $dhtfile
 
 $WshShell = New-Object -comObject WScript.Shell
 $Shortcut = $WshShell.CreateShortcut($desktop + "\Aria2下载器.lnk") 
@@ -130,6 +135,7 @@ if ($startUp -eq "y") {
     "CreateObject(`"WScript.Shell`").Run `"$dirpath\aria2c.exe --conf-path=$dirpath\config\Aria2Config.conf`",0" > Aria2StartUp.vbs
     (Get-Content trackerUpdateClient.ps1) -replace 'Replace', "`"$dirpath`"" | Set-Content .\trackerUpdateClient.ps1
     powershell .\trackerUpdateClient.ps1
+    Copy-Item .\Aria2StartUp.vbs $dirpath
     Copy-Item .\Aria2StartUp.vbs $startUpFolder
     #.\Aria2StartUp.ps1
 } else {
@@ -137,8 +143,9 @@ if ($startUp -eq "y") {
     Start-Sleep -Seconds 1
 }
 
+"`n请手动点击弹出的文件以完成第一次启动"
+
 #Set-Clipboard -Value "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe"
 Invoke-Item "$Env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup"
 
-"`n配置已经完成,enjoy"
 Pause
